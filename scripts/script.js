bb7fd4a06a3bb6fa6a7ff14654f94cb99aa1b13f
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-05-01 20:43:16 +0200

    divide script into modules and allow pause function with different audios

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,11 @@
+import { playNote } from "./functions.js";
+import { pauseNote } from "./functions.js";
+import { createText } from "./functions.js";
+import { toggleSwitch } from "./functions.js";
+
 /* ----- Sound and key animation ----- */
 
 const keys  = document.querySelectorAll(".key"); //NodeList [] 
-const volumeSlider = document.getElementById("volume-slider");
-
-let audioPlayed = 0;
-
-const playNote = (key) => {
-    const note = key.getAttribute("data-note");
-    const audio = new Audio(`../assets/note-sounds/${note}.mp3`);
-
-    //store audio in variable for pause
-    audioPlayed = audio;
-    audio.volume = volumeSlider.value;
-    audio.currentTime= 0;
-    audio.playbackRate = 1;
-    audio.play();
-    key.classList.add('active');
-
-    //Make long notes last more time
-    let speed;
-    let jump = 0.111111;
-    let interval = setInterval(() => {
-        speed = 1 - jump;
-        jump = Math.cbrt(jump);
-        audio.playbackRate = speed;
-        if (speed<=0.22 || audio.paused || audio.ended){
-            clearInterval(interval);
-        } }, 100);
-}
-
-const pauseNote = (key) => {
-    let audio = audioPlayed;
-    key.classList.remove('active');
-    if (!audio.paused) {
-        setTimeout(() => {
-            audio.muted = true;
-            audio.pause();}, 200)
-    }
-}
 
 //Click events in each button
 keys.forEach(key => {
@@ -71,42 +39,9 @@ document.addEventListener("keyup", function(event){
 
 /* ----- Letterrs and keyboard text ----- */
 
-const createText = (key) => {
-    const note = key.getAttribute("data-note");
-    const keyboard = key.getAttribute("data-key");
-    let keysContainer = document.createElement("span");
-    let notesContainer = document.createElement("span");
-    let lineBreak = document.createElement("br");
-    if (key.classList.contains('black')){
-        keysContainer.classList. add("black");
-        notesContainer.classList. add("black");
-    }
-    keysContainer.classList. add("keyText");
-    notesContainer.classList. add("noteText");
-    keysContainer.textContent = `${keyboard.toUpperCase()}`;
-    notesContainer.textContent = `${note}`;
-    key.appendChild(keysContainer);
-    key.appendChild(lineBreak);
-    key.appendChild(notesContainer);
-}
-
 keys.forEach(key => {
     createText(key);
 })
-
-const toggleSwitch = (keysText, checkbox) => {
-    for (const key of keysText){
-        if (checkbox.checked == true){
-            if (key.classList.contains('black')){
-                key.style.color = "white";
-            } else {
-                key.style.color = "black";
-            }
-        } else {
-            key.style.color = "transparent";
-        }
-    }
-}
 
 //Piano notes switch
 const noteCheckbox = document.getElementById("note-checkbox");
